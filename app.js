d8647b299ecec9c4a262bf98ea1d569745f0c5a6
YourName <noor11809027@gmail.com>
2022-08-31 16:45:07 +0600

    remove using splice

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ const app = Vue.createApp({
       this.goals.push(this.enteredGoal);
       this.enteredGoal="";
     },
+    removeGoal(index) {
+      this.goals.splice(index,1);
+     
+    },
   },
 });
 
